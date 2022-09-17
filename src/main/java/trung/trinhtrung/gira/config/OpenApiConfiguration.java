6133a9f44cbe14513be354a74d81f8ff45b8e86a
minhtrungcm181 <minhtrungcm181@gmail.com>
2022-09-17 15:44:21 +0700

    Initial

---
+++ src/main/java/trung/trinhtrung/gira/config/OpenApiConfiguration.java
@@ -0,0 +1,33 @@
+package trung.trinhtrung.gira.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI getOpenApi() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")))
+                .info(new Info()
+                        .title("Gira Application")
+                        .description("Service for Education Purpose")
+                        .version("v1.0")
+                        .license(new License().name("NO LICENSE").url(""))
+                        .contact(new Contact()
+                                .email("minhtrungcm181@gmail.com")
+                                .name("Trinh Minh Trung")
+                                .url("")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Spring Documentation")
+                        .url("https://docs.spring.io/spring-framework/docs/current/reference/html/"));
+    }
+
+}
